93e2f760f4cce594da5ddb1476c98d0fd3b14a26
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-11-06 11:30:15 -0600

    added create movie controller and added the app handling in index.js

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,7 +1,24 @@
+const db = require("../db.json");
+
 module.exports = {
   getMovies: (req, res) => {
-    console.log("We got a request");
+    res.status(200).send(db);
+  },
+  createMovie: (req, res) => {
+    const { title, rating, imageURL } = req.body;
 
-    res.sendStatus(200);
+    if (!title || !rating || !imageURL) {
+      return res.status(400).send("Missing one of the required fields");
+    }
+    //if the input cannot be cohorsed into a number then send this invalid code
+    if (isNaN(+rating)) {
+      return res.status(400).send("invalid input for rating.");
+    }
+
+    const newMovie = { title, rating, imageURL };
+
+    db.push(newMovie);
+
+    res.status(201).send(db);
   },
 };
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // this allows us to parse JSON into a javascript object that we access at req.body
 
-app.get("/api/movies");
+app.get("/api/movies", userCtrl.getMovies);
+app.post("/api/movies", userCtrl.createMovie);
 
 app.listen(PORT, () => console.log(`Server is running in port: ${PORT}`));
